c80ed0724c26ebceabe0668b5c98642696af7bba
Aleksander Ivanov <alexmmech@gmail.com>
2021-08-11 19:37:01 +0300

    neshto

--- src/App.js
+++ src/App.js
@@ -1,103 +1,104 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Home from '../src/component/Home';
-import Login from '../src/component/Please';
-import fire from '../src/component/fire';
-
+import Home from "./component/home/Home";
+import Login from "./component/login/Please";
+import fire from "./component/login/fire";
 
 const App = () => {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
 
+  const clearInuts = () => {
+    setEmail("");
+    setPassword("");
+  };
 
-    const [user, setUser] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const [hasAccount, setHasAccount] = useState(false);
+  const clearErrors = () => {
+    setPasswordError("");
+    setEmailError("");
+  };
 
-    const clearInuts = () => {
-        setEmail('');
-        setPassword('');
-    };
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
 
-    const clearErrors = () => {
-        setPasswordError('');
-        setEmailError('');
-    };
+  const handleSignup = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
 
-    const handleLogin = () => {
-        clearErrors();
-        fire
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .catch((err) => {
-            switch(err.code){
-                case "auth/invalid-email":
-                case "auth/user-disabled":
-                case "auth/user-not-found":
-                    setEmailError(err.message)
-                    break;
-                case "auth/wrong-password":
-                    setPasswordError(err.message)
-                    break;
-            };
-        })
-    }
+  const handleSignout = () => {
+    fire.auth().signOut();
+  };
 
-    const handleSignup = () => {
-        clearErrors();
-        fire
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .catch((err) => {
-            switch(err.code){
-                case "auth/email-already-in-use":
-                case "auth/invalid-email":
-                    setEmailError(err.message)
-                    break;
-                case "auth/weak-password":
-                    setPasswordError(err.message)
-                    break;
-            };
-        });
-    };
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInuts();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
 
-    const handleSignout = () => {
-        fire.auth().signOut();
-    };
+  useEffect(() => {
+    authListener();
+  }, []);
 
-    const authListener = () => {
-        fire.auth().onAuthStateChanged(user => {
-            if(user){
-                clearInuts();
-                setUser(user);
-            } else {
-                setUser('');
-            };
-        });
-    };
+  return (
+    <div className="App">
+      {user ? (
+        <Home handleSignout={handleSignout} />
+      ) : (
+        <Login
+          emai={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+          handleSignup={handleSignup}
+          hasAccount={hasAccount}
+          setHasAccount={setHasAccount}
+          emailError={emailError}
+          passwordError={passwordError}
+        />
+      )}
+    </div>
+  );
+};
 
-    useEffect(() => {
-        authListener();
-    },[]);
-
-    return (
-        <div className="App">
-            <Login 
-                emai={email}
-                setEmail={setEmail}
-                password={password}
-                setPassword={setPassword}
-                handleLogin={handleLogin}
-                handleSignup={handleSignup}
-                hasAccount={hasAccount}
-                setHasAccount={setHasAccount}
-                emailError={emailError} 
-                passwordError={passwordError}
-            />
-        </div>
-    )
-}
-
-export default App
+export default App;
--- src/component/login/Please.js
+++ src/component/login/Please.js
@@ -1,60 +1,92 @@
-import React from 'react'
+import React from "react";
 
-import './style.css'
+import "./style.css";
 
 const Please = (props) => {
-    const { email, 
-        setEmail, 
-        password, 
-        setPassword, 
-        handleLogin, 
-        handleSignup, 
-        hasAccount, 
-        setHasAccount, 
-        emailError,
-        passwordError} = props
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+    handleSignup,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError,
+  } = props;
 
-        return (
-        <section className="login">
-            <div className="loginContainer">
-                <label>Email</label>
-                <input
-                type="text"
-                autoFocus
-                autoCapitalize
-                value={email}
-                onChange={(e) => {
-                    setEmail(e.target.value);
-                }}
-                />
-                <p class="errorMsg">{emailError}</p>
-                <label>Password</label>
-                <input
-                    type="text"
-                    required
-                    value={password}
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                />
-                <p className="errorMsg">{passwordError}</p>
-                <div className="btnContainer">
-                    {hasAccount ? (
-                        <>
-                          <button onClick={handleLogin}>Sign in</button>
-                          <p>Dont have an account? <span onClick={() => setHasAccount(!hasAccount)}> Sign in </span> </p>
-                        </>
-                    ) : (
-                        <>
-                          <button onClick={handleSignup}>Sign up</button>
-                          <p>Have an account? <span onClick={() => setHasAccount(!hasAccount)}> Sign up </span> </p>
-                        </>    
-                       )}
-                </div>
-            </div>
+  const hidePass = () => {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  };
 
-        </section>
-    )
-}
+  return (
+    <section className="login">
+      <div className="loginContainer">
+        <label>Email</label>
+        <input
+          type="text"
+          autoFocus
+          autoCapitalize
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
 
-export default Please
+
+        <div onClick={hidePass} className="icon">
+        <i class="fas fa-eye-slash"></i>
+        </div>
+        <div onClick={hidePass} className="icon">
+          <i class="fas fa-eye"></i>
+        </div>
+
+        <p class="errorMsg">{emailError}</p>
+        <label>Password</label>
+        <input
+          id="myInput"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <p className="errorMsg">{passwordError}</p>
+        <div className="btnContainer">
+          {hasAccount ? (
+            <>
+              <button onClick={handleLogin}>Sign in</button>
+              <p>
+                Dont have an account?{" "}
+                <span onClick={() => setHasAccount(!hasAccount)}>
+                  {" "}
+                  Sign in{" "}
+                </span>{" "}
+              </p>
+            </>
+          ) : (
+            <>
+              <button onClick={handleSignup}>Sign up</button>
+              <p>
+                Have an account?{" "}
+                <span onClick={() => setHasAccount(!hasAccount)}>
+                  {" "}
+                  Sign up{" "}
+                </span>{" "}
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Please;
